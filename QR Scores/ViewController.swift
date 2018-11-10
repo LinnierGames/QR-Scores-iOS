420747f73f95baaf309fa60ab8872992da8dca87
LinnierGames <esericksanc@gmail.com>
2018-11-10 14:15:14 -0800

    Init Commit

---
+++ QR Scores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Scores
+//
+//  Created by Erick Sanchez on 11/10/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
