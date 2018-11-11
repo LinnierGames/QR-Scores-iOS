8a3a332513b2888976e532d040ecfd260a7d7334
LinnierGames <esericksanc@gmail.com>
2018-11-11 14:04:17 -0800

    WIP: DetailedSurveyVc

---
+++ QR Scores/SurveyDetailedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SurveyDetailedViewController.swift
+//  QR Scores
+//
+//  Created by Erick Sanchez on 11/11/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class SurveyDetailedViewController: UIViewController {
+
+    // MARK: - VARS
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
